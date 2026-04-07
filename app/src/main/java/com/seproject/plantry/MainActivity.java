e1171a142d83934675fa3b3a34665166402657c9
Lucy Burke <273163502+lucy-burke@users.noreply.github.com>
2026-04-07 03:08:53 -0500

    Added setTheme function to ensure that the correct day/night mode is loaded at app startup

--- app/src/main/java/com/seproject/plantry/MainActivity.java
+++ app/src/main/java/com/seproject/plantry/MainActivity.java
@@ -1,6 +1,10 @@
 package com.seproject.plantry;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,6 +23,8 @@ import com.google.android.material.navigation.NavigationView;
 import com.seproject.plantry.database.PantryDatabase;
 import com.seproject.plantry.database.PantryItem;
 import com.seproject.plantry.databinding.ActivityMainBinding;
+
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
     private PantryDatabase db;
@@ -29,9 +36,29 @@ public class MainActivity extends AppCompatActivity {
 
         // this.db = PantryDatabase.getInstance(getApplicationContext()); // make sure you have a singleton getter in AppDatabase
 
+        setTheme(getApplicationContext());
+
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
         BottomNavigationView navbar = findViewById(R.id.nav_toolbar);
         NavigationUI.setupWithNavController(navbar, navController);
+    }
+
+    /*
+    * Sets the theme of the app (ensures that it is actually set on start up!)
+    * */
+    private void setTheme(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        switch (preferences.getString("theme_selection", "system")) {
+            case "system":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case "dark":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case "light":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+        }
     }
 }
