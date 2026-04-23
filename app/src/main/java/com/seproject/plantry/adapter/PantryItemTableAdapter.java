8438956221fe95e4b5beebe931bfaf91f2560f4c
Lucy Burke <273163502+lucy-burke@users.noreply.github.com>
2026-04-23 11:05:33 -0500

    Initial (broken) upload

---
+++ app/src/main/java/com/seproject/plantry/adapter/PantryItemTableAdapter.java
@@ -0,0 +1,46 @@
+package com.seproject.plantry.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+
+import com.seproject.plantry.R;
+import com.seproject.plantry.database.PantryItem;
+
+public class PantryItemTableAdapter extends ListAdapter<PantryItem, PantryItemTableAdapter.PantryItemTableViewHolder> {
+    protected PantryItemTableAdapter() {
+        super(DIFF_CALLBACK);
+    }
+
+    private static final DiffUtil.ItemCallback<PantryItem> DIFF_CALLBACK =
+            new DiffUtil.ItemCallback<PantryItem>() {
+                @Override
+                public boolean areItemsTheSame(@NonNull PantryItem oldItem, @NonNull PantryItem newItem) {
+                    return oldItem.getId() == newItem.getId();
+                }
+
+                @Override
+                public boolean areContentsTheSame(@NonNull PantryItem oldItem, @NonNull PantryItem newItem) {
+                    return oldItem.equals(newItem);
+                }
+            };
+
+    @NonNull
+    @Override
+    public PantryItemTableAdapter.PantryItemTableViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_pantry, parent, false);
+        return new PantryItemTableAdapter.PantryItemTableViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PantryItemTableAdapter.PantryItemTableViewHolder holder, int position) {
+        PantryItem item = items.get(position);
+        holder.quantity.setText(String.valueOf(item.quantity));
+        holder.buy.setText(String.valueOf(item.buyDate));
+        holder.expiration.setText(String.valueOf(item.expirationDate));
+    }
+}
